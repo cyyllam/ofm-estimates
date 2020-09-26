6034a852921212043e916db0fe26417e5525684f
Christy Lam <cyyllam@gmail.com>
2020-09-26 00:11:51 -0700

    added delta report option

--- shiny/server.R
+++ shiny/server.R
@@ -3,19 +3,43 @@ server <- function(input, output) {
     years <- input$tablr_year
     id_cols <- c("Filter", "County", "Jurisdiction")
     
-    df %>% 
-      arrange(year) %>% 
-      filter(attr == input$tablr_attr,
-             year %in% seq(min(years), max(years))) %>% 
-      pivot_wider(id_cols = all_of(id_cols),
-                  names_from = year)
+    if (input$tablr_report_type == "Total") {
+      t <- df %>% 
+        arrange(year) %>% 
+        filter(attr == input$tablr_attr,
+               year %in% seq(min(years), max(years))) %>% 
+        pivot_wider(id_cols = all_of(id_cols),
+                    names_from = year)
+      return(t)
+    } else if (input$tablr_report_type == "Delta"){
+      t <- df %>% 
+        arrange(year) %>% 
+        filter(attr == input$tablr_attr,
+               year %in% seq(min(years), max(years))) %>% 
+        calc_delta() %>% 
+        pivot_wider(id_cols = all_of(id_cols),
+                    names_from = year,
+                    values_from = delta) %>%
+        ungroup()
+      return(t)
+    }
   })
   
   output$main_table <- renderReactable({
-    t <- filter_data() %>% 
-      select(-Filter)
+    t <- filter_data() %>% select(!Filter)
+    
     cols <- str_subset(colnames(t), "\\d{4}")
 
+    if (input$tablr_report_type == "Delta") {
+      # re-name column headers 
+      cols_tail_full <- tail(cols, -1)
+      cols_tail <- cols_tail_full %>% map(~ paste0("-", str_extract(.x, "\\d{2}$"))) %>% unlist
+      new_cols_name <- head(cols, -1) %>% paste0(cols_tail)
+      names(cols_tail_full) <- new_cols_name
+      t <- t %>% rename(!!!cols_tail_full)
+      cols <- c(cols[1], new_cols_name)
+    }
+    
     reactable(t,
               searchable = T,
               # defaultPageSize = ,
