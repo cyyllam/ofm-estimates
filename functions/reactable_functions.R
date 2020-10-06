77baa863242c17d72140754a85b903e6fb715696
Christy Lam <cyyllam@gmail.com>
2020-10-05 22:45:42 -0700

    added chromatic color to subcell

--- functions/reactable_functions.R
+++ functions/reactable_functions.R
@@ -19,7 +19,7 @@ make_color_pal <- function(colors, bias = 1) {
   function(x) rgb(get_color(x), maxColorValue = 255)
 }
 
-top_ten_color <- make_color_pal(c("#ffe4b2","#ffc966", "#ffa500"), bias = 1.3)
+top_ten_color <- make_color_pal(c("#ffe4b2","#ffc966", "#ffa500"), bias = 5)
 
 rt_default_col_def <- function(table, format_type, add_style_top_ten = F) {
   if (format_type == "number") {
@@ -32,6 +32,7 @@ rt_default_col_def <- function(table, format_type, add_style_top_ten = F) {
     return(colDef(align = 'center',
                   cell = function(value, index, name) {
                     x <- head(sort(unlist(table[[name]]), decreasing = T), 10)
+                    
                     if (format_type == "number") {
                       frmt <- format(value, big.mark=",")
                     } else if (format_type == "percent") {
@@ -39,24 +40,15 @@ rt_default_col_def <- function(table, format_type, add_style_top_ten = F) {
                     }
                     
                     if (is.numeric(value) && value != 0 && value %in% x) {
-                      div(class = "sub-cell", frmt)
+                      normalized <- (value - min(x)) / (max(x) - min(x))
+                      color <- top_ten_color(normalized)
+                      div(class = "sub-cell", style = list(background = color), frmt)
                     } else if (is.numeric(value)){
                       frmt
                     } else {
                       value
                     }
                   }
-                  # style = function(value, index, name) {
-                  #   x <- head(sort(unlist(table[[name]]), decreasing = T), 10)
-                  #   if (is.numeric(value) && value != 0 && value %in% x) {
-                  #     normalized <- (value - min(x)) / (max(x) - min(x))
-                  #     color <- top_ten_color(normalized)
-                  #     list(fontWeight = "300",
-                  #          background = color
-                  #          # borderRadius = "100%"
-                  #          )
-                  #   }
-                  # }
                   )
            )
   } else {
